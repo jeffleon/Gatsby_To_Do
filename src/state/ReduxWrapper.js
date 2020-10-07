4a6a1c561ea01261840f1e9b3d42eb3f9718fef6
Jefferson Leon <jeffersonleon1527@gmail.com>
2020-10-07 17:31:25 -0500

    [ADD] redux persist

--- src/state/ReduxWrapper.js
+++ src/state/ReduxWrapper.js
@@ -3,15 +3,23 @@ import { Provider } from "react-redux"
 import { createStore, applyMiddleware, compose } from "redux"
 import thunk from "redux-thunk"
 import rootReducer from "."
+import { persistStore, persistReducer } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+import storage from 'redux-persist/lib/storage'
 
 
 const initialState = {}
 const middleware = [thunk]
-
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   initialState,
   compose(applyMiddleware(...middleware))
 )
+var persistor = persistStore(store)
 store.subscribe(() => console.log(store.getState()));
-export default ({ element }) => <Provider store={store}>{element}</Provider>
+export default ({ element }) => <Provider store={store}><PersistGate persistor={persistor}>{element}</PersistGate></Provider>
